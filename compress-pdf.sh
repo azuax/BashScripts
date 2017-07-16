aede8a2a77e3431e0e9524c63f654c0324e9712a
Azuax <azuaxx@gmail.com>
2017-07-15 23:31:25 -0400

    Created Compress PDF  version 0.1

--- compress-pdf.sh
+++ compress-pdf.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
-directorio=finales
-if [ ! -d $directorio ]; then
-  mkdir $directorio
+echo "Please enter the directory where the files are [current directory] "
+read baseDir
+baseDir=${baseDir:-$(pwd)}
+
+echo "Please enter the directory where you want to put your compressed files [compress] "
+read compressDir
+compressDir=$baseDir"/"${compressDir:-"compress"}
+
+if [ ! -d $compressDir ]; then
+  mkdir $compressDir
 fi
+
+# ref: https://www.cyberciti.biz/tips/handling-filenames-with-spaces-in-bash.html
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
 i=0
-for f in *.pdf; do
-  gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$directorio/c_$f" "$f"
-  $((i++))
+for f in $baseDir"/"*.pdf; do
+  echo $f
+  gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$compressDir/$(basename $f)" "$f"
+  i=$((i + 1))
 done
 
 echo "it processed $i files"
+
+# restore $IFS
+IFS=$SAVEIFS
