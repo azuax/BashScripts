845fbe90fb1070e7029018dbaf125f91e99e7b30
Azuax <azuaxx@gmail.com>
2017-07-17 17:03:43 -0400

    Update para que tome archivos con extensión en mayúsculas

--- compress-pdf.sh
+++ compress-pdf.sh
@@ -15,12 +15,17 @@ fi
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
+# ignore case
+shopt -s nocaseglob
 i=0
 for f in $baseDir"/"*.pdf; do
   echo $f
   gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$compressDir/$(basename $f)" "$f"
   i=$((i + 1))
 done
+
+#redo case
+shopt -u nocaseglob
 
 echo "it processed $i files"
 
